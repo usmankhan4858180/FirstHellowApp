09d49e8a96fe38b5e9e23cf07c05ce45730ee8dd
DEll1 <usman.khan4858180@gmail.com>
2017-09-17 22:45:57 +0500

    my first app :)

---
+++ app/src/main/java/com/example/helloword/MainActivity_first.java
@@ -0,0 +1,13 @@
+package com.example.helloword;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity_first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_first);
+    }
+}
